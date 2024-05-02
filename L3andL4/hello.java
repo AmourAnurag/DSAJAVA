6d70d0263780fdff56cc3e7bcb93fc9b0eabdd91
AmourAnurag <anurag875636@gmail.com>
2024-05-02 09:08:03 +0530

    added notes

--- L3andL4/hello.java
+++
@@ -1,8 +0,0 @@
-package L3andL4;
-class hello{
-    public static void main(String[] args) {
-        
-        System.out.println("hello World");
-        System.out.println("hello1");
-    }
-}
---
+++ L4andL5/hello.java
@@ -0,0 +1,22 @@
+package L4andL5;
+public class hello{
+    public static void main(String[] args) {
+        
+        System.out.println("hello World");
+        System.out.println("hello1");
+     
+    }
+}
+class ram{
+    public static void main(String[] args) {
+        System.out.println("hello ram");
+    }
+}
+
+class shyam{
+    public static void main(String[] args) {
+        System.out.println("hello Shyam");
+    }
+}
+
+//in a java file we declare only one top level class means as public access specifier
