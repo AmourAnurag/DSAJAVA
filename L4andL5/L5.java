dabf6adfb96fba99ee7f66f8f85b19b305a19ac6
AmourAnurag <anurag875636@gmail.com>
2024-05-02 10:32:20 +0530

    L5 completed

---
+++ L4andL5/L5.java
@@ -0,0 +1,11 @@
+package L4andL5;
+
+public class L5 {
+    public static void main(String[] args) {
+        byte b1=(byte)(129);
+        System.out.println(b1);
+        short b2=(short)(50000);
+        System.out.println(b2);
+       
+    }
+}
