90989f7a49ad8ebfdf96eb267a0f11ed12e717b9
AmourAnurag <anurag875636@gmail.com>
2024-07-06 08:02:01 +0530

    concept of string

---
+++ L49toL51/L50.java
@@ -0,0 +1,42 @@
+package L49toL51;
+
+import java.util.Scanner;
+
+public class L50 {
+    public static void main(String args[]){
+        //concept of string formating
+        // Scanner sc=new Scanner(System.in);
+        // System.out.println("Enter the first word ");
+        // String word=sc.next();
+        // System.out.println("Enter the second word ");
+        // String word1=sc.next();
+        // System.out.println("Enter the first line ");
+        // sc.nextLine();
+        // String line=sc.nextLine();
+        // System.out.println("Enter the second line ");
+        // String line1=sc.nextLine();
+
+        // System.out.println("First word "+word);
+        // System.out.println("Second word "+word1);
+        // System.out.println("First line "+line);
+        // System.out.println("Secon line "+line1)  ;      
+
+
+        //concatnation of strings when we add string with integer double it convert in to string
+        // System.out.println("Ram is a "+56);
+        // System.out.println(45+" you are");
+        // System.out.println((45+96)+" gentleman");
+        // System.out.println(" gentleman"+45+96);
+        // System.out.println(" gentleman"+(45+96));
+
+
+
+        //string formatting 
+        int rollNum=8569;
+
+        String name="  Ram  ";
+        System.out.println(name);
+        String data=String.format("Name is %2$s and age is %1$d", rollNum,name);
+        System.out.println(data);
+    }
+}
