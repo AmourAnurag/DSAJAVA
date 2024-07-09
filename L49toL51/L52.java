6b7cc27015115e88bfe2cf382479311e9d5c83c5
AmourAnurag <anurag875636@gmail.com>
2024-07-09 08:57:01 +0530

    leetcode question average waiting time

--- L49toL51/L52.java
+++ L49toL51/L52.java
@@ -55,11 +55,46 @@ public class L52 {
         }
         System.out.println("Yes palindrome");
     }
+    static void isPalindrome(String s) {
+        String str[]=s.split("[! ,?._'@]+:");
+        StringBuilder ans=new StringBuilder();
+        for(int i=0;i<str.length;i++){
+            ans.append(str[i]);
+        }
+        System.out.println(ans.toString());
+    }
 
+    static double averageWaitingTime(int[][] customers) {
+        int n =customers.length;
+        int count=0;
+        int minus=0;
+        double avg=0;
+        int sumOfWait=customers[0][0];
+        for(int i=0;i<n;i++){
+            // System.out.println(sumOfWait);
+            count++;
+            // for(int j=0 ; j<customers[i].length;j++){
+                sumOfWait=sumOfWait+customers[i][1];
+                if (sumOfWait<customers[i][0]) {
+                    avg=avg+((customers[i][0]+customers[i][1])-customers[i][0]);
+                    continue;
+                }
+                minus=sumOfWait-customers[i][0];
+                avg=avg+minus;
+                System.out.println(avg);
+            
+        }
+        System.out.println(avg + " "+count);
+        double average=(avg/count);
+        return average;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the String ");
-        String str = sc.next();
-        palindrome2(str);
+        // System.out.println("Enter the String ");
+        // String str = sc.next();
+
+        int[][] customers = {{5,2},{5,4},{10,3},{20,1}};
+        double avg=averageWaitingTime(customers);
+        System.out.println(avg);
     }
 }
