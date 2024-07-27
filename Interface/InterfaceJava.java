a1645f91cdbba1be51b241bda53cedcd24a1c604
AmourAnurag <anurag875636@gmail.com>
2024-07-27 08:29:58 +0530

    interface in java

---
+++ Interface/InterfaceJava.java
@@ -0,0 +1,29 @@
+package Interface;
+
+interface Bike{
+    void print();
+}
+class Honda implements Bike{
+   @Override
+   public void print() {
+       System.out.println("Bike is running at the port 8001");
+   }
+   void printHello(){
+    System.out.println("this is the methos of the honda class");
+   }
+}
+interface Hero extends Bike{
+  @Override
+  default void print() {
+      System.out.println("Hero bike is the best bike in india");
+  }
+
+}
+public class InterfaceJava implements Hero {
+     public static void main(String arg[]){
+        Honda hd=new Honda();
+        hd.print();
+        InterfaceJava sc=new InterfaceJava();
+        sc.print();
+     }
+}
